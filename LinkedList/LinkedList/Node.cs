22407b420b73d4267f632e4612e8947b18b746c2
VengalreddyAkhila <akhila.it1801@gmail.com>
2021-06-16 21:37:25 +0530

    Added:CreatedList

---
+++ LinkedList/LinkedList/Node.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LinkedList
+{
+    /// <summary>
+    /// creating the list 
+    /// </summary>
+    public class Node
+    {
+        public int data;
+        public Node next;
+        public Node (int i)
+        {
+            data = i;
+            next = null;
+        }
+        public void Print()
+        {
+            Console.WriteLine("" + data + "");
+           if (next != null)
+           {
+                next.Print();
+           }
+        }
+    }
+}
+ 
---
+++ LinkedList/LinkedList/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace LinkedList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Linkedlist");
+            Node node = new Node(56);
+            node.next = new Node(30);
+            node.next.next = new Node(70);
+            node.Print();
+        }
+    }
+}
