14a5aed4c80fd60b7842f3af7801ae2c9055f8fe
VengalreddyAkhila <akhila.it1801@gmail.com>
2021-06-16 23:13:45 +0530

    Refactor:UC3-InsertingData

--- LinkedList/LinkedList/Node.cs
+++ LinkedList/LinkedList/Node.cs
@@ -7,14 +7,18 @@ namespace LinkedList
     /// <summary>
     /// creating the list 
     /// </summary>
+   
     public class Node
     {
         public int data;
         public Node next;
+        private Node head;
         public Node(int i)
         {
+            
             data = i;
             next = null;
+            head = null;
         }
         /// <summary>
         /// Adding the data to list
@@ -27,7 +31,60 @@ namespace LinkedList
             else
                 next.Add(data);
         }
+        /// <summary>
+        /// inserts the data at starting position in list
+        /// </summary>
+        public void InsertAtstart()
+        {
+            Node temp = new Node(data);
+            temp.next = head;
+            head = temp;
+        }   
+        /// <summary>
+        /// inserts the data at particulat position
+        /// </summary>
+        /// <param name="data"></param>
+        /// <param name="k"></param>        
+        public void Insertatparticular(int data, int k)
+        {          
+               Node  temp;
+            if (k == 1)
+            {
+                temp = new Node(data);
+                temp.next = head;
+                head = temp;
+                return;
+            }
+                        
+               Node p = head;
+            for (int i = 1; i < k - 1 && p != null; i++)
+            {
+                p = p.next;
+                if (p == null)
+                    Console.WriteLine("" + i + "");
+                else
+                {
+                    temp = new Node(data);
+                    temp.next = p.next;
+                    p.next = temp;
+                }
+            }
 
+        }           
+        
+        //prints the list after insertion
+        public void Show()
+        {
+            Node node = head;
+            while (node.next != null)
+            {
+                Console.WriteLine(node.data);
+                node = node.next;
+            }
+            Console.WriteLine(node.data);
+        }
+       
+        //printing the list
         public void Print()
         {
             Console.WriteLine("" + data + "");
@@ -35,6 +92,7 @@ namespace LinkedList
             {
                 next.Print();
             }
+
         }
     }
     
